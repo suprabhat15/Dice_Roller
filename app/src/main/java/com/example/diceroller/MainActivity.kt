cc1e03a30d5934e6b0b29836f02583d45f640504
suprabhat25 <suprabhat2018@gmail.com>
2020-05-10 18:09:54 +0530

    Add countUp() to increment the count

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,18 +7,40 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener{countUp()}
     }
-    val randomInt = (1..6).random()
+
         private fun rollDice(){
            // Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+            val randomInt = (1..6).random()
             val resultText: TextView = findViewById(R.id.result_text)
             //resultText.text = getString(R.string.dice_rolled)
             resultText.text = randomInt.toString()
         }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        if(resultText.text == "Hello World"){
+            resultText.text ="1"
+        }
+        else{
+            var resultInt = resultText.text.toString().toInt()
+
+            if(resultInt < 6){
+                resultInt = resultInt + 1
+                resultText.text = resultInt.toString()
+            }
+
+        }
+
+    }
 }

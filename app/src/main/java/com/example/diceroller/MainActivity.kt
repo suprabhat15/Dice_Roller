c84bc4c326b4db96e8ac4e72372e0896ff04e402
suprabhat25 <suprabhat2018@gmail.com>
2020-05-11 13:13:36 +0530

    Add 2 Images replacing the only Image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,12 +12,14 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
    lateinit var diceImage: ImageView
+   lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
@@ -26,10 +28,15 @@ class MainActivity : AppCompatActivity() {
     }
 
         private fun rollDice(){
+            diceImage.setImageResource(getRandomDiceImage())
+            diceImage2.setImageResource(getRandomDiceImage())
+        }
+
+        private fun getRandomDiceImage(): Int{
             val randomInt = (1..6).random()
 //            val diceImage: ImageView = findViewById(R.id.dice_image)
 
-            val drawableResource = when (randomInt){
+            return when (randomInt){
                 1 -> R.drawable.dice_1
                 2 -> R.drawable.dice_2
                 3 -> R.drawable.dice_3
@@ -37,9 +44,6 @@ class MainActivity : AppCompatActivity() {
                 5 -> R.drawable.dice_5
                 else -> R.drawable.dice_6
             }
-
-            diceImage.setImageResource(drawableResource)
-
         }
 
 }
